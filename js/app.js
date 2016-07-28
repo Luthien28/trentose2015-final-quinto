cfaf6119ff21a07236d7cba1d8a0a7d7691b7e6e
Luthien28 <linda.bertox@gmail.com>
2016-07-28 10:09:58 +0200

    Non si refresha input e testo case sensitive

--- js/app.js
+++ js/app.js
@@ -3,25 +3,43 @@
  * over a collection of questions
  */
 var TriviaModel = {
-  
+  current:0,
    /* Collection of available questions */
    collection : questions, 
   
    /* Start the iteration over the collection of questions.
       It sets the first element as the current question */    
    init : function() {
+	   console.log(questions);
+	   $("#question").text(questions[TriviaModel.current].question);
    },
   
    /* Set the next element as the current question. 
       Returns true if it moved successfully to the next,
               false if there were no more elements */
    next : function(){
+	   console.log("bottone premuto");
+	   if(TriviaModel.current==questions.length) {  window.alert("No more question.");
+									 return false;
+									}
+	//	   return false;
+	  var risposta = document.getElementById("answer").value;
+	  TriviaModel.isAnswerCorrect(risposta);
+	   return true;
+	   
    },
   
    /* Returns the current question 
     *         null if the collection was not initiated
     */
    getCurrentQuestion: function () { 
+	   console.log(TriviaModel.current);
+	   console.log(questions.length);
+       if(TriviaModel.current<questions.length) {
+		   TriviaModel.current++;
+           $("#question").text(questions[TriviaModel.current].question); }
+	   console.log(TriviaModel.current);
+	   console.log(questions[TriviaModel.current].question);
    },
   
    /* Checks if the answer provided is correct. The comparison
@@ -30,6 +48,16 @@ var TriviaModel = {
               false if not
     */
    isAnswerCorrect : function (answer){
+	   
+	   console.log(answer);
+	   if(answer == questions[TriviaModel.current].answer) {
+		   window.alert("Correct");
+		    TriviaModel.getCurrentQuestion();
+	   }	else window.alert("Incorrect");
+	   $("#answer").refresh;
+	  
+	   
    }  
            
 };
+TriviaModel.init();
--- test/spec/app.spec.js
+++ test/spec/app.spec.js
@@ -3,8 +3,11 @@
 
 describe("Trivia", function() {
 
-  it("should...", function() {  
-    
-  });    
+  it("tell you if your anwers are correct", function() {  
+	  expect(isAnswerCorrect("In which year were the first Modern Olympics held?")).toBe("1896")
+      it(" should tells you if there are no more questions")
+  });    , function { expect(next()).not.toBe(null);
+	  
+  }
  
 });
